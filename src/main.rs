7baaa805bf72512963fe4b30eb0c41e91c26e9f6
Username * <oueslatim@pm.me>
2025-03-20 12:12:38 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
+mod camera;
+mod food;
+mod grid;
+mod player;
+
 use bevy::prelude::*;
 
+use camera::CameraPlugin;
+use food::FoodPlugin;
+use grid::GridPlugin;
+use player::PlayerPlugin;
+
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: (1920., 1080.).into(),
+                title: "B3D".into(),
+                visible: true,
+                resizable: false,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_plugins((PlayerPlugin, CameraPlugin, GridPlugin, FoodPlugin))
+        .run();
 }
