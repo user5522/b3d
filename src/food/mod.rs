e7ff7339fb50d944e87e41791b9b1a8661a061d0
Username * <oueslatim@pm.me>
2025-03-20 12:04:34 +0100

    setup "food" eat spawn system
    food is a temp name, probably

---
+++ src/food/mod.rs
@@ -0,0 +1,30 @@
+use bevy::prelude::*;
+
+mod components;
+mod systems;
+
+use systems::*;
+
+pub const FOOD_COLOR: Color = Color::srgb(0.9, 0.3, 0.3);
+
+pub struct FoodPlugin;
+
+impl Plugin for FoodPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(
+            Startup,
+            |commands: Commands,
+             meshes: ResMut<Assets<Mesh>>,
+             materials: ResMut<Assets<StandardMaterial>>| {
+                spawn_food(
+                    commands,
+                    // default player position, probably should be a constant somewhere
+                    &Transform::from_xyz(0.0, 0.5, 0.0),
+                    meshes,
+                    materials,
+                )
+            },
+        )
+        .add_systems(Update, food_consumed);
+    }
+}
---
+++ src/food/systems.rs
@@ -0,0 +1,72 @@
+use bevy::prelude::*;
+use rand::prelude::*;
+
+use crate::{
+    grid::{GRID_CELL_SIZE, GRID_DEPTH, GRID_WIDTH},
+    player::components::Player,
+};
+
+use super::{components::Food, *};
+
+pub fn spawn_food(
+    mut commands: Commands,
+    player_transform: &Transform,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let mut rng = rand::rng();
+    let mut new_pos;
+    let player_transform = player_transform;
+
+    loop {
+        new_pos = Vec2::new(
+            rng.random_range(-1 * (GRID_WIDTH / 2) as i32..(GRID_WIDTH / 2) as i32) as f32,
+            rng.random_range(-1 * (GRID_DEPTH / 2) as i32..(GRID_DEPTH / 2) as i32) as f32,
+        );
+
+        if new_pos
+            != Vec2::new(
+                player_transform.translation.x,
+                player_transform.translation.y,
+            )
+        {
+            break;
+        }
+    }
+
+    let food_mesh = meshes.add(Cuboid::default());
+    let food_material = materials.add(StandardMaterial {
+        base_color: FOOD_COLOR,
+        ..default()
+    });
+
+    commands.spawn((
+        Transform::from_xyz(new_pos.x * GRID_CELL_SIZE, 0.5, new_pos.y * GRID_CELL_SIZE),
+        GlobalTransform::default(),
+        Mesh3d(food_mesh),
+        MeshMaterial3d(food_material),
+        Food,
+    ));
+}
+
+pub fn food_consumed(
+    mut commands: Commands,
+    mut player: Query<(&Transform, &mut Player), With<Player>>,
+    mut food: Query<(Entity, &Transform), (With<Food>, Without<Player>)>,
+    meshes: ResMut<Assets<Mesh>>,
+    materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let (player_transform, mut player) = player.single_mut();
+
+    for (food_entity, food_transform) in food.iter_mut() {
+        let distance = player_transform
+            .translation
+            .distance(food_transform.translation);
+
+        if distance < GRID_CELL_SIZE / 2.0 {
+            commands.entity(food_entity).despawn();
+            spawn_food(commands, &player_transform, meshes, materials);
+            break;
+        }
+    }
+}
